942abb3c63b18f9830413a7f9f4651f4381c95f8
N Athul Kumar <athulnvinod@gmail.com>
2021-05-07 15:37:54 +0530

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.querySelector('.toggle').addEventListener('click',()=>{
+    document.querySelector('.toggle').classList.toggle('active');
+})
+
+document.querySelector('.toggle').addEventListener('click',()=>{
+    document.querySelector('.showcase').classList.toggle('active');
+})
+
+document.querySelector('.toggle').addEventListener('click',()=>{
+    document.querySelector('.menu').classList.toggle('active');
+})
